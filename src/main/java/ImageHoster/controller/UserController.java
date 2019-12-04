a0d7afdbd4d1d9439138c2e30433a232fb84cc05
Pradeep Kumar <kumarpradeep@vmware.com>
2019-12-04 19:15:34 +0530

    Bug Fix : Tags not shown on edit button click

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -40,12 +40,12 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user,Model model) {
-        String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+    public String registerUser(User user, Model model) {
         if (passwordStrength(user.getPassword())) {
             userService.registerUser(user);
             return "users/login";
         } else {
+            String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
             model.addAttribute("User", user);
             model.addAttribute("passwordTypeError", error);
             return "users/registration";
